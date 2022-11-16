dc4bde777b044708c5bc2ee1ab2c53a6a834fae2
farazmirsaeedi <farazmirsaeedi@gmail.com>
2022-11-16 19:03:20 +0330

    init commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var result = document.getElementById("res");
+let value=0
+const counter = ()=>{
+    value++;
+    document.getElementById("res").innerHTML=value;
+} 
+
+const counter1 = ()=>{
+    value--;
+    document.getElementById("res").innerHTML=value;
+    } 
+    
+
+function click(){
+        alert("hi");
+    }
+    
