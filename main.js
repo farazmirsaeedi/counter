58ac55ca766bbb95d29c4b1812b9d481d45ffe4f
farazmirsaeedi <farazmirsaeedi@gmail.com>
2023-05-03 21:25:49 +0430

    add dynamic style

--- main.js
+++ main.js
@@ -4,17 +4,30 @@ let value = 0
 const counter = () => {
     value++;
     document.getElementById("res").innerHTML = value;
+    color()
+
 }
 
 const counter1 = () => {
     value--;
     document.getElementById("res").innerHTML = value;
+    color()
+
 }
 
 
 const rest = () => {
     value = 0;
     document.getElementById("res").innerHTML = value;
+    color()
+
 }
 
+const color = () => {
+    if (result.innerHTML < 0) {
+        result.style.color = "red";
+    } else {
+        result.style.color = "black";
 
+    }
+}
